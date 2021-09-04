b610f94c04939b1d7c15f1d49cd732f1dd4cf121
YusukeNakatsubo <nakatsubo@mbp.local>
2021-09-04 12:55:43 +0900

    Create 2-8.

---
+++ chapter02/2_8/app.js
@@ -0,0 +1,32 @@
+// ロードされ、Vueがグローバル変数として定義されているか確認
+console.assert(typeof Vue !== 'undefined');
+
+let items = [
+  { name: '鉛筆', price: 300, quantity: 0 },
+  { name: 'ノート', price: 400, quantity: 0 }, 
+  { name: '消しゴム', price: 500, quantity: 0 }
+];
+
+var vm = new Vue({
+  el: '#app',
+  data: {
+    items: items
+  },
+  filters: {
+    numberWithDelimiter: function (value) {
+      if (!value) { return '0' }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  computed: {
+    totalPrice: function() {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function() {
+      return Math.floor(this.totalPrice * 1.08)
+    }
+  }
+})
+window.vm = vm
